e1c407765d9991b63837cf5c9bc6f4030aec0f49
William Rodriguez <william@rodriguez1.com>
2022-04-25 22:34:15 -0700

    Simple interaction between discord and vscode

--- extension.js
+++ extension.js
@@ -1,12 +1,39 @@
 const vscode = require('vscode');
+const { Client, Intents } = require('discord.js');
+const path = require('path');
+require("dotenv").config({ path: path.resolve(__dirname, './.env') });
+
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
-//let outputTest;
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 async function activate(context) {
+	const token = process.env.BOT_KEY;
+	
+	client.once('ready', c => {
+		console.log(`Ready! Logged in as ${c.user.tag}`);
+	});
+	
+	client.on('interactionCreate', async interaction => {
+		if (!interaction.isCommand()) return;
+	
+		const { commandName } = interaction;
+	
+		if (commandName === 'ping') {
+			await interaction.reply('Pong!');
+		} else if (commandName === 'server') {
+			await interaction.reply('Server info.');
+		} else if (commandName === 'user') {
+			await interaction.reply('User info.');
+		}
+	});
+	
+	
+	client.login(token);
 
 	console.log('Congratulations, your extension "parrot" is now active!');
 
@@ -19,18 +46,13 @@ async function activate(context) {
 
 	//outputTest = vscode.window.createOutputChannel("parrotOutput");
 
-	context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(displayMessage));
-	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(displayMessage));
+	// context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(displayMessage));
+	// context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(displayMessage));
 
 	//setInterval(displayMessage, 1000);
 }
 
 function deactivate() {}
-
-function displayMessage() {
-	let message = "test";
-	vscode.commands.executeCommand("parrot.helloWorld", message);
-}
 
 module.exports = {
 	activate,
--- parrot.js
+++ parrot.js
@@ -1,87 +1,54 @@
-import Discord from 'discord.io';
 import {} from 'dotenv/config'
 
-let selectedGuild;
-let selectedChan = "966012063568789557";
-let parrotChan = "968243023970467901";
-let commandChan = "968243052139405404";
+// let selectedGuild;
+// let selectedChan = "966012063568789557";
+// let parrotChan = "968243023970467901";
+// let commandChan = "968243052139405404";
 
 
-let bot = new Discord.Client({
-  token: process.env.BOT_KEY,
-  autorun: true
-});
+  // if (message.substring(0,1) === '!') {
+  //   let args = message.substring(1).split(" ");
+  //   let cmd = args[0];
 
-bot.on('ready', function() {
-  console.log('Logged in as %s - %s\n', bot.username, bot.id);
-});
-
-bot.on('message', function (user, userId, channelId, message, evt) {
-  // if(message.author.bot) {
-  //   return;
+  //   args = args.splice(1);
+  //   switch(cmd) {
+  //     case "test":
+  //     bot.sendMessage({
+  //       to: channelId,
+  //       message: `This test is working. channelId is ${channelId} avatar is ${evt.d.author.avatar} user is: ${user}user ID is ${userId}`
+  //     });
+  //     break;
+  //     case "setChan":
+  //       selectedChan = channelId;
+  //       bot.sendMessage({
+  //         to: channelId,
+  //         message: `This channel is selected for messaging`
+  //       });
+  //     break;
+  //     case "setCom":
+  //       commandChan = channelId;
+  //       bot.sendMessage({
+  //         to: commandChan,
+  //         message: "this channel is selected for commands"
+  //       });
+  //   }
   // }
 
-  // console.log(evt.d.author.bot);
-  if (message.substring(0,1) === '!') {
-    let args = message.substring(1).split(" ");
-    let cmd = args[0];
+  // if(channelId === selectedChan && userId !== bot.id) {
+  //   bot.sendMessage({
+  //     to: parrotChan,
+  //     message: `${user}: ${message}`
+  //   });
+  // } else if (channelId === selectedChan && message.substring(0,1) !== '!') {
+  //   bot.sendMessage({
+  //     to: parrotChan,
+  //     message: `${message}`
+  //   });
+  // }
 
-    args = args.splice(1);
-    switch(cmd) {
-      case "test":
-      bot.sendMessage({
-        to: channelId,
-        message: `This test is working. channelId is ${channelId} avatar is ${evt.d.author.avatar} user is: ${user}user ID is ${userId}`
-      });
-      break;
-      case "setChan":
-        selectedChan = channelId;
-        bot.sendMessage({
-          to: channelId,
-          message: `This channel is selected for messaging`
-        });
-      break;
-      case "setCom":
-        commandChan = channelId;
-        bot.sendMessage({
-          to: commandChan,
-          message: "this channel is selected for commands"
-        });
-    }
-  }
-
-  if(channelId === selectedChan && userId !== bot.id) {
-    bot.sendMessage({
-      to: parrotChan,
-      message: `${user}: ${message}`
-    });
-  } else if (channelId === selectedChan && message.substring(0,1) !== '!') {
-    bot.sendMessage({
-      to: parrotChan,
-      message: `${message}`
-    });
-  }
-
-  if(channelId === commandChan && userId !== bot.id) {
-    bot.sendMessage({
-      to: selectedChan,
-      message: `${user}: ${message}`
-    });
-  }
-  
-
-
-
-});
-
-
-
-
-function vsMessage(channel, vsMessage) {
-  bot.sendMessage({
-    to: channel,
-    message: `${vsMessage}`
-  });
-}
-
-console.log(process.env.TEST_KEY);
+  // if(channelId === commandChan && userId !== bot.id) {
+  //   bot.sendMessage({
+  //     to: selectedChan,
+  //     message: `${user}: ${message}`
+  //   });
+  // }
